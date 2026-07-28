e03f8fa5123879bd126904bb14df593775d21a16
smallarry78-web <smallarry78@gmail.com>
2026-07-27 22:51:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ async def rss_worker():
                     f"Found {len(articles)} new article(s)."
                 )
 
-                       for article in articles:
+            for article in articles:
 
                 success = await Publisher.publish(
                     bot,
@@ -124,6 +124,7 @@ async def rss_worker():
                 )
 
                 if success:
+
                     db.mark_published(
                         article["id"],
                         article["channel"]
